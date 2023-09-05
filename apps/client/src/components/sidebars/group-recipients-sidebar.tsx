8ce0265a2e4415bd76da8aed581d953f3559962c
sherifzaher <sherifashrafmedia@gmail.com>
2023-09-06 02:12:09 +0300

    ping websocket to receive online & offline users

--- apps/client/src/components/sidebars/group-recipients-sidebar.tsx
+++ apps/client/src/components/sidebars/group-recipients-sidebar.tsx
@@ -1,6 +1,7 @@
 import { PeopleGroup } from 'akar-icons';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import {
   GroupRecipientSidebarItem,
   GroupRecipientSidebarItemContainer,
@@ -9,14 +10,36 @@ import {
   MessageItemAvatar
 } from '../../utils/styles';
 import { RootState } from '../../store';
+import { useSocketContext } from '../../context/socket-context';
 
 export default function GroupRecipientsSidebar() {
-  const { id } = useParams();
-
+  const { id: groupId } = useParams();
   const group = useSelector((state: RootState) => state.groups.groups).find(
-    (groupItem) => groupItem.id === parseInt(id!, 10)
+    (groupItem) => groupItem.id === parseInt(groupId!, 10)
   );
-  console.log(group);
+  const socket = useSocketContext();
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([]);
+  const [offlineUsers, setOfflineUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    socket.emit('getOnlineGroupUsers', { groupId });
+    const interval = setInterval(() => {
+      console.log(`Pinging Group ${groupId}`);
+      socket.emit('getOnlineGroupUsers', { groupId });
+    }, 10000);
+    socket.on('onlineGroupUsersReceived', (payload) => {
+      console.log('received payload for online users');
+      console.log(payload);
+      setOnlineUsers(payload.onlineUsers);
+      setOfflineUsers(payload.offlineUsers);
+    });
+    return () => {
+      console.log('Clearing Interval for GroupRecipientsSidebar');
+      clearInterval(interval);
+      socket.off('onlineGroupUsersReceived');
+    };
+  }, [groupId, socket]);
+
   return (
     <GroupRecipientsSidebarStyle>
       <GroupRecipientsSidebarHeader>
@@ -24,7 +47,15 @@ export default function GroupRecipientsSidebar() {
         <PeopleGroup />
       </GroupRecipientsSidebarHeader>
       <GroupRecipientSidebarItemContainer>
-        {group?.users.map((user) => (
+        <span>Online Users</span>
+        {onlineUsers.map((user) => (
+          <GroupRecipientSidebarItem key={user.id}>
+            <MessageItemAvatar />
+            <span>{user.firstName}</span>
+          </GroupRecipientSidebarItem>
+        ))}
+        <span>Offline Users</span>
+        {offlineUsers.map((user) => (
           <GroupRecipientSidebarItem key={user.id}>
             <MessageItemAvatar />
             <span>{user.firstName}</span>
