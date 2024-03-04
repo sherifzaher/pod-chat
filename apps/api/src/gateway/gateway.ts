0566b77ce1efc8104165c83d5363cf5eb75abd76
sherifzaher <sherifashrafmedia@gmail.com>
2024-03-04 03:08:03 +0200

    api: listen to videoCallRejected event and emit onVideoCallRejected

--- apps/api/src/gateway/gateway.ts
+++ apps/api/src/gateway/gateway.ts
@@ -350,4 +350,16 @@ export class MessagingGateway
       socket.emit('onVideoCallAccept', payload);
     }
   }
+
+  @SubscribeMessage('videoCallRejected')
+  async handleVideoCallRejected(
+    @MessageBody() data,
+    @ConnectedSocket() socket: AuthenticatedSocket,
+  ) {
+    console.log('inside videoCallRejected event');
+    const receiver = socket.user;
+    const callerSocket = this.sessions.getSocketId(data.caller.id);
+    callerSocket && callerSocket.emit('onVideoCallRejected', { receiver });
+    socket.emit('onVideoCallRejected', { receiver });
+  }
 }
